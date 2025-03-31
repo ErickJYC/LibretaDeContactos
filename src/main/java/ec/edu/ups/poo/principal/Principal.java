9a2d31c9ceecd9d7e3eabc0626d0784490a59f9f
Crakchi <crakchi@gmail.com>
2025-03-31 01:09:50 -0500

    FIX(BFRZ-UPS-002): Limpieza de buffer

--- src/main/java/ec/edu/ups/poo/principal/Principal.java
+++ src/main/java/ec/edu/ups/poo/principal/Principal.java
@@ -1,5 +1,5 @@
 package ec.edu.ups.poo.principal;
-//Hola
+
 import ec.edu.ups.poo.clases.Familiar;
 import ec.edu.ups.poo.clases.Persona;//se importa porque estan en paquetes diferentes
 
@@ -10,21 +10,12 @@ public class Principal {
     public static void main(String[] args) {
 
         Persona persona = new Persona();
-        persona.setCedula("02002020");
-        persona.setNombre("Jose");
-        persona.setApellido("Perez");
-        persona.setDireccion("Loja");
-
-
-
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Ingreseel numero de familiares");
+        System.out.println("Ingrese el numero de familiares");
         int numeroFamilares = scanner.nextInt();
         Familiar[] familiares = new Familiar[numeroFamilares];
-        scanner.nextLine();
         for (int i = 0; i < numeroFamilares; i++) {
-
+            scanner.nextLine();
             System.out.println("Ingrese el numero de cedula");
             String cedula = scanner.nextLine();
             System.out.println("Ingrese el nombre");
@@ -49,17 +40,6 @@ public class Principal {
             Familiar familiar = new Familiar(nombre, apellido, cedula, direccion, tipoDeSangre, parentesco, fechaDeNacimiento);
             familiares[i] = familiar;
         }
-        // Imprimir sin toString
-//        for (int i = 0; i <numeroFamilares;i++){
-//            System.out.println(familiares[i].getCedula());
-//            System.out.println(familiares[i].getNombre());
-//            System.out.println(familiares[i].getApellido());
-//            System.out.println(familiares[i].getDireccion());
-//            System.out.println(familiares[i].getParentesco());
-//            System.out.println(familiares[i].getTipoDeSangre());
-//            System.out.println(familiares[i].calcularEdad(familiares[i].getFechaDeNacimiento()));
-//        }
-        //Imprimir con el toString
 
         for (int i = 0; i < numeroFamilares; i++) {
             System.out.println(familiares[i].toString());
@@ -67,12 +47,12 @@ public class Principal {
 
         System.out.println("Ingrese el numero de personas: ");
         int numeroPersonas = scanner.nextInt();
-        scanner.nextLine();
+
         Persona[] personas = new Persona[numeroPersonas];
         for (int i = 0; i < numeroPersonas; i++) {
 
-            Persona persona = new Persona();
             System.out.println("--------------------------------------------------------------------------------------");
+            scanner.nextLine();
             System.out.println("Ingrese el numero de cedula");
             String cedula = scanner.nextLine();
             System.out.println("Ingrese el nombre");
@@ -90,15 +70,6 @@ public class Principal {
 
             personas[i] = persona;
         }
-        // Imprimir sin el toString
-//        for (int i = 0; i < numeroPersonas; i++) {
-//            System.out.println("--------------------------------------------------------------------------------------");
-//            System.out.println(personas[i].getCedula());
-//            System.out.println(personas[i].getNombre());
-//            System.out.println(personas[i].getApellido());
-//            System.out.println(personas[i].getDireccion());
-//        }
-        //Imprimir con el toString
         for (int i = 0; i < numeroPersonas; i++) {
             System.out.println(personas[i].toString());
         }
